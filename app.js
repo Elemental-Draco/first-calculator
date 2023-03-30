cc38180b6b917444e424efa90f47751cd77aa035
Elemental-Draco <jdwashington31@gmail.com>
2023-03-29 21:10:02 -0400

    redid the functionality of the operations, there  was a bug with the orignial code, would display NaN if user input a number with more than 2 digits as operand. last thing to do is add functionality for when the user presses another operator instead of equals.

---
+++ app.js
@@ -0,0 +1,80 @@
+// DOM ELEMENTS
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const equals = document.querySelector("[data-id='equals']");
+const decimal = document.querySelector("[data-id='decimal']");
+const del = document.querySelector("[data-id='delete']");
+const clear = document.querySelector("[data-id='clear']");
+const display = document.querySelector(".displayInput");
+
+// math functions
+
+function compute(firstVal, operation, secondVal) {
+  switch (operation) {
+    case "+":
+      return +firstVal + +secondVal;
+    case "-":
+      return +firstVal - +secondVal;
+    case "X":
+      return +firstVal * +secondVal;
+    case "/":
+      return +firstVal / +secondVal;
+  }
+}
+
+let firstValue;
+let operationSign;
+let secondValue;
+// event listeners -----------------------
+
+// event listener for all the buttons
+
+numbers.forEach((num) => {
+  num.addEventListener("click", (event) => {
+    display.innerText += event.target.innerText;
+  });
+});
+
+operators.forEach((sign) => {
+  sign.addEventListener("click", (event) => {
+    if (
+      /[+\-\/X]/.test(display.innerText) == false &&
+      display.innerText !== "" &&
+      display.innerText !== "."
+    ) {
+      display.innerText += event.target.innerText;
+    } else if (/[+\-\/X]/.test(display.innerText)) {
+    }
+  });
+});
+// del key done
+del.addEventListener("click", () => {
+  // if (
+  //   !isNaN(display.innerText.charAt(display.innerText.length - 1)) ||
+  //   display.innerText.charAt(display.innerText.length - 1) === "."
+  // ) {
+  display.innerText = display.innerText.slice(0, -1);
+  // }
+});
+
+// Clear button done
+clear.addEventListener("click", () => {
+  display.innerText = "";
+  firstValue = 0;
+  operationSign = "";
+  secondValue = 0;
+});
+// Decimal point Done
+decimal.addEventListener("click", () => {
+  if (!display.innerText.includes(".")) {
+    display.innerText += ".";
+  }
+});
+
+equals.addEventListener("click", (event) => {
+  let positionOfOperator = display.innerText.search(/[+\-\/X]/);
+  firstValue = display.innerText.slice(0, positionOfOperator);
+  secondValue = display.innerText.slice(positionOfOperator + 1);
+  operationSign = display.innerText.charAt(positionOfOperator);
+  display.innerText = compute(firstValue, operationSign, secondValue);
+});
